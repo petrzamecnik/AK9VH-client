da74eeb40364db583fded6e7ed752ac507d74259
Petr Zamecnik <Petazamecek@gmail.com>
2024-10-26 18:27:18 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.setMenu(null); // Hides the default menu
+    win.loadFile('src/index.html'); // Adjust the path if necessary
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
